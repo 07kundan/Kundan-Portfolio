d930cf008d266767e9307561ad6ee90b12f8f87a
kundan <kundankumar28884@gmail.com>
2024-07-25 12:48:52 +0530

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
+import "./app.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="bg-red-500">hello</div>;
+  return (
+    <div className="h-screen flex items-center justify-center text-2xl text-emerald-600 font-bold">
+      <div className="inline-flex items-end gap-2">
+        <span className="">Under Progress </span>
+        <div class="loading-dots">
+          <div class="dot"></div>
+          <div class="dot"></div>
+          <div class="dot"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
